902ddde21ad1c59acfd5b6d209f69f9fea5f017d
Alex Hermundson <alex.hermundson@gmail.com>
2017-02-28 22:44:14 -0600

    added react and react dom and stubbed in test render

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
+import React from 'react';
+import { render } from 'react-dom';
 import storeFactory from './store'
 import { addPlayer, removePlayer, addOpponent, gameDate } from './actions'
+
+window.React = React
+
+render(
+  <h1>Test</h1>,
+  document.getElementById('react-container')
+);
 
 const initialState = localStorage['redux-store'] ? JSON.parse(localStorage['redux-store']) : {}
 
